1ee4a022b17d95894aa9ecf2258cd892d5572b70
Christopher Lang <christopher.lang.256@gmail.com>
2024-05-14 15:07:02 +0100

    Add loading to search page

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
+  @override
+  SearchPageState createState() => SearchPageState();
+}
+
+class SearchPageState extends State<SearchPage> {
+  bool loading = false;
+
+  @override
+  void initState() {
+    loadRegion("Cambridge");
+  }
+  void loadRegion(String location) async {
+    setState(() { this.loading = true; });
+    /* This is where the api call will take place. */
+    await Future.delayed(Duration(seconds: 3));
+    /* Results from api call will be passed to home page. */
+    Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+    /*
+     * Wait a bit before removing loading text so we dont change before home
+     * page has covered up search page.
+     */
+    await Future.delayed(Duration(milliseconds: 500));
+    setState(() { this.loading = false; });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,12 +36,16 @@ class SearchPage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: this.loading
+          ? <Widget>[
+            Text('Loading'),
+          ]
+          : <Widget>[
             Text('Search Page Demo Text'),
             ElevatedButton(
-              child: Text('Return'),
+              child: Text('Cambridge'),
               onPressed: () {
-                Navigator.pop(context);
+                loadRegion("Cambridge");
               },
             ),
           ],
