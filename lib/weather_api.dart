7cba06d4171a35da7ec1a6724996b4984794b4d3
SamuelSey <samuel05seyoum@gmail.com>
2024-05-16 15:13:29 +0100

    Comments

--- lib/weather_api.dart
+++ lib/weather_api.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'dart:async';
 
 /*
+Example of  how to use getWeather method
+
 void main() async {
   final weatherApi = WeatherApi();
   final weatherData = await weatherApi.getWeather('Cambridge');
@@ -14,39 +16,17 @@ class WeatherApi {
   static Future<Map<String, dynamic>> getWeather(String location) async {
     final response = await http.get(Uri.parse(
         'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location},UK?unitGroup=uk&key=6N2KTB9AP7VU5JTN5WRKUNXPC'));
+    // The response uses asynchronous processing to wait for the request to return
+    // This is using the Visual Crossing Weather API (My API key) with a location (in the UK) in town name form
 
     if (response.statusCode == 200) {
+      // If response is valid
       final decoded = jsonDecode(response.body) as Map<String, dynamic>;
+      // Breaks JSON file returned into a dictionary
       return decoded;
     } else {
       throw Exception('Failed to load data: ${response.statusCode}');
+      // In event of request not working, return error code
     }
   }
 }
-
-  /*
-  static Future<Map<String, dynamic>> fetchData() async {
-    await WeatherApi.getWeather();
-    final jsonString = """
-{
-  "morning": {"clouds": "HAPPY", "frogs": "SAD"},
-  "afternoon": {"clouds": "HAPPY", "frogs": "SAD"}
-}
-    """;
-    return jsonDecode(jsonString) as Map<String, dynamic>;
-  }
-
-  static String exampleRequest() {
-
-    final jsonString = """
-{
-  "morning": {"clouds": "HAPPY", "frogs": "SAD"},
-  "afternoon": {"clouds": "HAPPY", "frogs": "SAD"}
-}
-    """;
-    final data = jsonDecode(jsonString) as Map<String, dynamic>;
-
-    return data['morning']['clouds'];
-
-   */
-
